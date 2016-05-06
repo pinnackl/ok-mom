a26d7d3971661596cb860c7489f785a0588e34ae
patak <pataky@hotmail.fr>
2016-05-06 12:58:55 +0200

    Template for OkMom

--- db/migrate/20160506084247_add_devise_to_users.rb
+++ db/migrate/20160506084247_add_devise_to_users.rb
@@ -45,7 +45,5 @@ class AddDeviseToUsers < ActiveRecord::Migration
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
     raise ActiveRecord::IrreversibleMigration
-    remove_column :users, :password
-    remove_column :users, :createdAt
   end
 end
