afc9cf177dcc494af61b0fd9b9ca6ca8c41fa9a5
patak <pataky@hotmail.fr>
2016-07-16 01:07:30 +0200

    improve css part 2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ActiveRecord::Base
 	belongs_to :child, class_name:'User', foreign_key: "child_id"
 	belongs_to :mother, class_name:'User', foreign_key: "mother_id"
+
+  	validates :title, presence: true
+  	validates :mother, presence: true
 end
